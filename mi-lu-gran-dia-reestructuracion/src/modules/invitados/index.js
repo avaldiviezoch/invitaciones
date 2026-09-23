77f5c66b253258f74cc7dd15d314392e6ef543bc
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-09-23 17:58:46 -0500

    chore(invitados): actualizar controlador RSVP

--- mi-lu-gran-dia-reestructuracion/src/modules/invitados/index.js
+++ mi-lu-gran-dia-reestructuracion/src/modules/invitados/index.js
@@ -1,6 +1,6 @@
 import { weddingCapabilities } from '../../core/app/permissions.js';
 import { loadInvitadosSnapshot, saveInvitadosSnapshot } from './invitados-data.js?v=3';
-import { createRsvpController } from './rsvp-controller.js?v=1';
+import { createRsvpController } from './rsvp-controller.js?v=2';
 import { createTablesController } from './tables-controller.js?v=15';
 
 let activeContext = null;
