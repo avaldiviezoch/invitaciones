d018ad6631eb1d29cf5521e6de118e5ce7ca9616
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-09-23 17:52:09 -0500

    Actualiza geometría responsive de Mesas

--- mi-lu-gran-dia-reestructuracion/src/modules/invitados/tables-controller.js
+++ mi-lu-gran-dia-reestructuracion/src/modules/invitados/tables-controller.js
@@ -1,5 +1,5 @@
 import { saveInvitadosSnapshot } from './invitados-data.js?v=3';
-import { normalizeTableShape, tableSeatGeometry } from './table-geometry.js?v=3';
+import { normalizeTableShape, tableSeatGeometry } from './table-geometry.js?v=4';
 
 const SHAPE_LABELS = Object.freeze({
   round: 'Redonda',
