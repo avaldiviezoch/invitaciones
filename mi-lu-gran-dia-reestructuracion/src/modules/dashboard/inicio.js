6ff98ccff4da7c342ace54069d6569a4b3617cf8
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-09-23 14:11:39 -0500

    Actualiza carga de política de modales

--- mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
+++ mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
@@ -1,6 +1,6 @@
 import { weddingCapabilities } from '../../core/app/permissions.js';
 import { mountChecklist } from '../checklist/index.js?v=13';
-import { mountPresupuesto } from '../presupuesto/index.js?v=ios-autofill-1';
+import { mountPresupuesto } from '../presupuesto/index.js?v=dialog-policy-1';
 import { auth } from '../../services/firebase-client.js';
 import {
   listWeddingContexts,
