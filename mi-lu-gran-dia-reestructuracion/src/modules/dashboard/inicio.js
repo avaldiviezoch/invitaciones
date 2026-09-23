6462d353f81d64abfde87a23e2ac9e8d88c66028
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-09-23 13:29:17 -0500

    Actualiza carga de Presupuesto

--- mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
+++ mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
@@ -1,6 +1,6 @@
 import { weddingCapabilities } from '../../core/app/permissions.js';
 import { mountChecklist } from '../checklist/index.js?v=13';
-import { mountPresupuesto } from '../presupuesto/index.js?v=7';
+import { mountPresupuesto } from '../presupuesto/index.js?v=8';
 import { auth } from '../../services/firebase-client.js';
 import {
   listWeddingContexts,
