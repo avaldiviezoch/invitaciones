a375efd6e847c6b41a3b24f2f9ebdfec15a334ed
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-09-23 17:52:12 -0500

    Actualiza controlador visual de Mesas

--- mi-lu-gran-dia-reestructuracion/src/modules/invitados/index.js
+++ mi-lu-gran-dia-reestructuracion/src/modules/invitados/index.js
@@ -1,7 +1,7 @@
 import { weddingCapabilities } from '../../core/app/permissions.js';
 import { loadInvitadosSnapshot, saveInvitadosSnapshot } from './invitados-data.js?v=3';
 import { createRsvpController } from './rsvp-controller.js?v=1';
-import { createTablesController } from './tables-controller.js?v=14';
+import { createTablesController } from './tables-controller.js?v=15';
 
 let activeContext = null;
 let snapshot = null;
