2d4682208f14cf832ac751a4dcce2cedc407da71
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-09-22 22:06:47 -0500

    Actualiza versión auditada del Checklist

--- mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
+++ mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
@@ -1,5 +1,5 @@
 import { weddingCapabilities } from '../../core/app/permissions.js';
-import { mountChecklist } from '../checklist/index.js?v=10';
+import { mountChecklist } from '../checklist/index.js?v=11';
 import { auth } from '../../services/firebase-client.js';
 import {
   listWeddingContexts,
