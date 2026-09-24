df920ab755ee298e0d0a2b9d1f2fef36a055c871
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-09-24 10:21:58 -0500

    Cargar cámara modular de Distribución

--- mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
+++ mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
@@ -637,7 +637,7 @@ async function openModule(moduleId, { updateHash = true } = {}) {
       await mountInvitados(weddingContext);
     }
     if (moduleId === 'distribucion') {
-      const { mountDistribucion } = await import('../distribucion/index.js?v=45');
+      const { mountDistribucion } = await import('../distribucion/index.js?v=46');
       await mountDistribucion(weddingContext);
     }
     if (loadEpoch === moduleLoadEpoch) {
