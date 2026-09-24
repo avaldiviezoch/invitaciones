a9253a5a0a217160832351530bf33c5bb1665992
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-09-24 10:29:09 -0500

    Cargar blindaje de propuestas e historial

--- mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
+++ mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
@@ -637,7 +637,7 @@ async function openModule(moduleId, { updateHash = true } = {}) {
       await mountInvitados(weddingContext);
     }
     if (moduleId === 'distribucion') {
-      const { mountDistribucion } = await import('../distribucion/index.js?v=48');
+      const { mountDistribucion } = await import('../distribucion/index.js?v=49');
       await mountDistribucion(weddingContext);
     }
     if (loadEpoch === moduleLoadEpoch) {
