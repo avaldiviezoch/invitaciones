1730cb2a72d5db5aa0a72e9eb514249f2ba72220
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-09-24 10:30:48 -0500

    Cargar protección de sillas canónicas

--- mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
+++ mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
@@ -637,7 +637,7 @@ async function openModule(moduleId, { updateHash = true } = {}) {
       await mountInvitados(weddingContext);
     }
     if (moduleId === 'distribucion') {
-      const { mountDistribucion } = await import('../distribucion/index.js?v=49');
+      const { mountDistribucion } = await import('../distribucion/index.js?v=50');
       await mountDistribucion(weddingContext);
     }
     if (loadEpoch === moduleLoadEpoch) {
