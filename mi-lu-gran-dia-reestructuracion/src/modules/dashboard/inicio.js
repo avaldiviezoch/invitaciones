ba54c9e77e7f528edb424efad6db355f5c1d433e
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-09-24 14:56:42 -0500

    chore(shell): publish faster Distribucion loading

--- mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
+++ mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
@@ -672,7 +672,7 @@ async function openModule(moduleId, { updateHash = true } = {}) {
       await mountInvitados(weddingContext);
     }
     if (moduleId === 'distribucion') {
-      const { mountDistribucion } = await import('../distribucion/index.js?v=74');
+      const { mountDistribucion } = await import('../distribucion/index.js?v=75');
       await mountDistribucion(weddingContext);
     }
     if (loadEpoch === moduleLoadEpoch) {
