376eaa59b1eb9cc66d66858ef55378b382890ae4
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-09-24 08:45:29 -0500

    Cargar inicio de Distribución Fase 5

--- mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
+++ mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
@@ -637,7 +637,7 @@ async function openModule(moduleId, { updateHash = true } = {}) {
       await mountInvitados(weddingContext);
     }
     if (moduleId === 'distribucion') {
-      const { mountDistribucion } = await import('../distribucion/index.js?v=7');
+      const { mountDistribucion } = await import('../distribucion/index.js?v=8');
       await mountDistribucion(weddingContext);
     }
     if (loadEpoch === moduleLoadEpoch) {
