8f075cbf18386ee1ae0bab29bc6ce72867312c52
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-09-25 14:42:22 -0500

    Actualizar caché de controles de rotación

--- mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
+++ mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
@@ -617,7 +617,7 @@ const MODULES = Object.freeze({
     mount: 'mountInvitados'
   },
   distribucion: {
-    load: () => import('../distribucion/index.js?v=114'),
+    load: () => import('../distribucion/index.js?v=115'),
     mount: 'mountDistribucion'
   }
 });
