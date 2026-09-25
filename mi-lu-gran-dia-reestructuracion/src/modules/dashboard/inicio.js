bdb6dbec1305abf7801e454e8e32af428ae4efc6
avaldiviezoch <155109177+avaldiviezoch@users.noreply.github.com>
2026-09-25 13:22:22 -0500

    Actualizar caché de Distribución tras Fase 9

--- mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
+++ mi-lu-gran-dia-reestructuracion/src/modules/dashboard/inicio.js
@@ -617,7 +617,7 @@ const MODULES = Object.freeze({
     mount: 'mountInvitados'
   },
   distribucion: {
-    load: () => import('../distribucion/index.js?v=104'),
+    load: () => import('../distribucion/index.js?v=105'),
     mount: 'mountDistribucion'
   }
 });
